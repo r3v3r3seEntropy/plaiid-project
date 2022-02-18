0f36f15093b02c80f57e3dc9d7bdb2640c53400a
Vatsal Sharma <vatsalsharma376@gmail.com>
2022-02-18 23:40:27 +0530

    register page design changes

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -67,77 +67,95 @@ class Register extends Component {
             <p className="text-gray-500">Register for ClaimYourAid.com now:</p>
             <br />
           </div>
-          <div
-            style={{ borderColor: borderColor }}
-            className="row max-w-xl w-full m-auto bg-white box-border border-4 rounded p-5"
-          >
-            <div className="col s8 offset-s2">
-              <form noValidate onSubmit={this.onSubmit}>
-                <div className="input-field col s12">
-                  <label htmlFor="email" className="block mb-2 text-gray-500">
-                    Email
-                  </label>
+          <div className="container flex content-center ">
+            <div
+              style={{ borderColor: borderColor }}
+              className="row max-w-xl w-full m-auto bg-white box-border border-4 rounded p-5"
+            >
+              <div className="col s8 offset-s2">
+                <form noValidate onSubmit={this.onSubmit}>
+                  <div className="input-field col s12">
+                    <label htmlFor="email" className="block mb-2 text-gray-500">
+                      Email
+                    </label>
 
-                  <input
-                    onChange={this.onChange}
-                    value={this.state.email}
-                    error={errors.email}
-                    id="email"
-                    type="email"
-                    className={classnames(
-                      "w-full p-2 mb-6 text-green-700 border-2 outline-none",
+                    <input
+                      onChange={this.onChange}
+                      value={this.state.email}
+                      error={errors.email}
+                      id="email"
+                      type="email"
+                      className={classnames(
+                        "w-full p-2 mb-6 text-green-700 border-2 outline-none",
+                        {
+                          invalid: errors.email,
+                        }
+                      )}
+                    />
+                    <span className="text-red-600">{errors.email}</span>
+                  </div>
+                  <div className="input-field col s12">
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-gray-500"
+                    >
+                      {"Password (6 or more letters & numbers)"}
+                    </label>
+
+                    <input
+                      onChange={this.onChange}
+                      value={this.state.password}
+                      error={errors.password}
+                      id="password"
+                      type="password"
+                      className={classnames(
+                        "w-full p-2 mb-6 text-green-700 border-2 outline-none",
+                        {
+                          invalid: errors.password,
+                        }
+                      )}
+                    />
+                    <span className="text-red-600">{errors.password}</span>
+                  </div>
+
+                  <div className="col" style={{ paddingLeft: "11.250px" }}>
+                    <p
+                      className="text-xs text-center"
+                      style={{ width: "100%" }}
+                    >
                       {
-                        invalid: errors.email,
+                        "By clicking Agree & Join, you agree to the ClaimYourAid User "
                       }
-                    )}
-                  />
-                  <span className="text-red-600">{errors.email}</span>
-                </div>
-                <div className="input-field col s12">
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-gray-500"
-                  >
-                    {"Password (6 or more letters & numbers)"}
-                  </label>
-
-                  <input
-                    onChange={this.onChange}
-                    value={this.state.password}
-                    error={errors.password}
-                    id="password"
-                    type="password"
-                    className={classnames(
-                      "w-full p-2 mb-6 text-green-700 border-2 outline-none",
-                      {
-                        invalid: errors.password,
-                      }
-                    )}
-                  />
-                  <span className="text-red-600">{errors.password}</span>
-                </div>
-
-                <div className="col" style={{ paddingLeft: "11.250px" }}>
-                  <p className="text-xs text-center" style={{ width: "50%" }}>
-                    {
-                      "By clicking Agree & Join, you agree to the ClaimYourAid User Agreement, Privacy Policy, and Cookie Policy."
-                    }
-                  </p>
-                  <button
-                    style={{
-                      width: "150px",
-                      borderRadius: "3px",
-                      letterSpacing: "1.5px",
-                      marginTop: "1rem",
-                      backgroundColor: "#00B050",
-                    }}
-                    type="submit"
-                    className="w-full hover:bg-pink-700 text-white font-bold py-2 px-4 mb-4 rounded"
-                  >
-                    Agree {"&"} Join
-                  </button>
-                </div>
-                {/* <p className="grey-text text-darken-1">
+                    </p>
+                    <p
+                      className="text-xs text-center"
+                      style={{ width: "100%" }}
+                    >
+                      {" Agreement, Privacy Policy, and Cookie Policy."}
+                    </p>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <button
+                        style={{
+                          width: "150px",
+                          borderRadius: "3px",
+                          letterSpacing: "1.5px",
+                          marginTop: "1rem",
+                          backgroundColor: "#00B050",
+                        }}
+                        type="submit"
+                        className="w-full hover:bg-pink-700 text-white font-bold py-2 px-4 mb-4 rounded"
+                      >
+                        Agree {"&"} Join
+                      </button>
+                    </div>
+                  </div>
+                  {/* <p className="grey-text text-darken-1">
                   Already have an account?{" "}
                   <Link
                     to="/login"
@@ -146,7 +164,8 @@ class Register extends Component {
                     Log in
                   </Link>
                 </p> */}
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
--- client/src/components/auth/Register2.js
+++ client/src/components/auth/Register2.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
 import LogoHeader from "../layout/LogoHeader";
+const borderColor = "#A2C987";
 class Register2 extends Component {
   constructor() {
     super();
@@ -55,7 +56,7 @@ class Register2 extends Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <LogoHeader />
         <div className="ml-20">
           <p>Thank you for registering for ClaimYourAid.com!</p>
@@ -63,68 +64,76 @@ class Register2 extends Component {
           <p className="mt-10 text-gray-500">
             Next, may we request your details
           </p>
-          <form noValidate onSubmit={this.onSubmit}>
-            <div className="input-field col s12">
-              <label htmlFor="fname" className="block mb-2 text-green-500">
-                First Name
-              </label>
+        </div>
+        <div className="container flex content-center ">
+          <div
+            style={{ borderColor: borderColor }}
+            className="row max-w-xl w-full m-auto bg-white box-border border-4 rounded p-5"
+          >
+            <form noValidate onSubmit={this.onSubmit}>
+              <div className="input-field col s12">
+                <label htmlFor="fname" className="block mb-2 text-gray-500">
+                  First Name
+                </label>
 
-              <input
-                onChange={this.onChange}
-                value={this.state.name}
-                id="fname"
-                type="text"
-                className={classnames(
-                  "w-full p-2 mb-6 text-green-700 border-b-2 border-green-500 outline-none focus:bg-gray-300"
-                )}
-              />
-            </div>
-            <div className="input-field col s12">
-              <label htmlFor="lname" className="block mb-2 text-green-500">
-                Last Name
-              </label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.name}
+                  id="fname"
+                  type="text"
+                  className={classnames(
+                    "w-full p-2 mb-6 text-green-700 border-2 outline-none"
+                  )}
+                />
+              </div>
+              <div className="input-field col s12">
+                <label htmlFor="lname" className="block mb-2 text-gray-500">
+                  Last Name
+                </label>
 
-              <input
-                onChange={this.onChange}
-                value={this.state.email}
-                id="lname"
-                type="text"
-                className={classnames(
-                  "w-full p-2 mb-6 text-green-700 border-b-2 border-green-500 outline-none focus:bg-gray-300"
-                )}
-              />
-            </div>
-            <div className="input-field col s12">
-              <label htmlFor="cell" className="block mb-2 text-green-500">
-                Cell Phone number
-              </label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  id="lname"
+                  type="text"
+                  className={classnames(
+                    "w-full p-2 mb-6 text-green-700 border-2 outline-none"
+                  )}
+                />
+              </div>
+              <div className="input-field col s12">
+                <label htmlFor="cell" className="block mb-2 text-gray-500">
+                  Cell Phone number
+                </label>
 
-              <input
-                onChange={this.onChange}
-                value={this.state.password}
-                id="cell"
-                type="text"
-                className={classnames(
-                  "w-full p-2 mb-6 text-green-700 border-b-2 border-green-500 outline-none focus:bg-gray-300"
-                )}
-              />
-            </div>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  id="cell"
+                  type="text"
+                  className={classnames(
+                    "w-full p-2 mb-6 text-green-700 border-2 outline-none"
+                  )}
+                />
+              </div>
 
-            <div className="col" style={{ paddingLeft: "11.250px" }}>
-              <button
-                style={{
-                  width: "150px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  marginTop: "1rem",
-                }}
-                type="submit"
-                className="w-full bg-green-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-1 rounded"
-              >
-                Click here to continue
-              </button>
-            </div>
-          </form>
+              <div className="col m-auto" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "450px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem",
+                    backgroundColor: "#00B050",
+                  }}
+                  type="submit"
+                  className="w-full hover:bg-pink-700 text-white font-bold py-2 px-4 mb-4 rounded"
+                >
+                  <center>Click Here To Continue</center>
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
